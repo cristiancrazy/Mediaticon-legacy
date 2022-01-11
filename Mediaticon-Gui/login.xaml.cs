72eb1c6a8ea69c8b8e18c29d76f8eef8dfd90801
Visual Laser 10 New <visualaser10new@gmail.com>
2022-01-11 23:24:09 +0000

    Add version 2 of gui
    I can finally think this is the almost final version of gui

---
+++ Mediaticon-Gui/login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mediaticon
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void nameTxt_GotFocus(object sender, RoutedEventArgs e)
+        {
+            nameTxt.SelectAll();
+        }
+
+        private void nameTxt_IsMouseCaptureWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            nameTxt.SelectAll();
+        }
+    }
+}
