e5ce8a3de526b09f4e6c447403fb86fd1c5408da
Visual Laser 10 New <visualaser10new@gmail.com>
2022-01-13 05:31:49 +0000

    first versions of app
    with backend

---
+++ Mediaticon-App/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using HandlerMediaticon;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mediaticon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    
+
+
+    public partial class MainWindow : Window
+    {
+        void openform<T>(bool close) where T : Window, new()
+        {
+            T a = new T();
+            a.Show();
+            if(close)
+                this.Close();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Film> listafilm = new List<Film>();
+
+            for(int i =0 ;i< 10; i++)
+                listafilm.Add(new Film("gino " + i, "il film dell'anno", ""));
+
+            listaLB.ItemsSource = listafilm;
+
+            for (int i = 0; i < 30; i++)
+                filterCBL.Items.Add(new CheckBox { Content=$"ciao{i}"});
+
+            openform<login>(false);
+            openform<details>(false);
+            openform<mylist>(false);
+        }
+
+        private void searchTxt_IsMouseCaptureWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            searchTxt.SelectAll();
+        }
+
+        private void accountBord_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            accountCombo.IsDropDownOpen = !accountCombo.IsDropDownOpen;
+        }
+    }
+}
---
+++ Mediaticon-App/details.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mediaticon
+{
+    /// <summary>
+    /// Interaction logic for details.xaml
+    /// </summary>
+    /// 
+
+    public class ProgTV
+    {
+        public ProgTV(string icon, DateTime orainizio, DateTime orafine)
+        {
+            giorno = orainizio.ToString("d");
+            oraInizio = orainizio.ToString("HH:mm");
+            oraFine = orafine.ToString("HH:mm");
+        }
+        public string giorno { set; get; }
+        public string oraInizio { set; get; }
+        public string oraFine { set; get; }
+    }
+
+    public partial class details : Window
+    {
+        void setCover()
+        {
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri(@"https://pad.mymovies.it/filmclub/2021/12/001/locandina.jpg");
+            img.EndInit();
+            coverImg.Source = img;
+        }
+
+        void setBackground()
+        {
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri(@"https://pad.mymovies.it/filmclub/2021/12/001/covermd_home.jpg");
+            img.EndInit();
+            bkgImg.ImageSource = img;
+        }
+
+        void makeList()
+        {
+            List<ProgTV> tVs = new List<ProgTV>();
+            for (int i = 0; i < 10; i++)
+            {
+                tVs.Add(new ProgTV("",
+                  new DateTime(year: 1, month: 1, day: 1, hour: 12, minute: 23+i, second: 0),
+                  new DateTime(year: 1, month: 1, day: 1, hour: 13, minute: 40+i, second: 0))
+                  );
+            }
+
+            listaTV.ItemsSource = tVs;
+        }
+
+        public details()
+        {
+            InitializeComponent();
+            setCover();
+            setBackground();
+            makeList();
+        }
+
+        private void mainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            
+        }
+
+        private void accountBord_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            accountCombo.IsDropDownOpen = !accountCombo.IsDropDownOpen;
+        }
+
+        private void comboClick(object sender, MouseButtonEventArgs e)
+        {
+            seeBtn.IsDropDownOpen = true;
+        }
+    }
+}
