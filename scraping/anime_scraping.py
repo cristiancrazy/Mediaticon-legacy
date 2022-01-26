1de441c1e4192e64402f434d4f2a0f812141fb3b
Giova-Bell <67321618+Giova-Bell@users.noreply.github.com>
2022-01-26 09:58:47 +0100

    error in taking year

--- scraping/anime_scraping.py
+++ scraping/anime_scraping.py
@@ -1,4 +1,5 @@
-import requests, re, bs4
+import requests, re, bs4, time
+from datetime import date
 from dataclasses import dataclass, field
 
 '''
@@ -26,23 +27,37 @@ class Anime:
     actors_list : list[str] = field(default_factory=list)
 
     def __str__(self):
-        return f'{self.tipo}\n{self.name} | {self.anno} -> {self.episodes}\n* {self.image}\n\n{self.trama}\n\n{self.tags}\n{self.actors_list}'
+        return f'{self.image};{self.name};{self.trama};{self.episodes};{self.anno};{self.tags};{self.tipo};{self.actors_list}'
 
 def myanilist():
-    letters = [ '.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    _from_index = 0
-    _to_index = 1
+    #letters = [ '.', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    _from_year = 1970
+    _to_year = 0
+
+    #CURRENT YEAR
+    today_date = date.today()
+    current_year = int(today_date.year)
 
     page = 0
     tipo = 'Anime'
 
-    for letter in letters[_from_index:_to_index]:
+    while True:
         #GET HTML
-        response = requests.get(f'https://myanimelist.net/anime.php?letter={letter}&show={page}')
-        response.raise_for_status() # give an error if the page returns an error code
+        #https://myanimelist.net/anime.php?cat=anime&q=&type=0&score=0&status=0&p=0&r=0&sm=0&sd=0&sy=1970&em=0&ed=0&ey=2022&c%5B0%5D=d&show=50
+        #response = requests.get(f'https://myanimelist.net/anime.php?letter={letter}&show={page}')
+        try:
+            response = requests.get(f'https://myanimelist.net/anime.php?cat=0&q=&type=0&score=0&status=0&p=0&r=0&sm=0&sd=0&sy={_from_year}&em=0&ed=0&ey={_to_year}&c%5B0%5D=d&o=2&w=2&show={page}')
+            response.raise_for_status() # give an error if the page returns an error code
+        except:
+            break
 
         #PREPARE FOR PARSING
         soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+        #CHECK IF END PAGE
+        # check = soup.find('div', {'id' : 'content'})
+        # if('No titles that matched your query were found.' in check.text):
+        #     break
 
         #GET NEEDED HTML
         animes = soup.find_all('tr')
@@ -70,7 +85,8 @@ def myanilist():
                     lateral_bar = soup.find('td', {'class' : 'borderClass'})
                     
                     #IMAGE
-                    anime.image = lateral_bar.find('img')['data-src']
+                    if(image := lateral_bar.find('img')):
+                        anime.image = image['data-src']
 
                     menu_info = soup.find_all('div', {'class' : 'spaceit_pad'})
                     for info in menu_info:
@@ -92,14 +108,24 @@ def myanilist():
                                 anime.tags.append(genre.text)
                     
                     #TRAMA
-                    anime.trama = soup.find('p', {'itemprop' : 'description'}).text.strip()
+                    anime.trama = soup.find('p', {'itemprop' : 'description'}).text.strip().replace(';', '§').replace('\n', ' ')
 
                     #ACTORS
-                    
+                    if(actors_list := soup.find('div', {'class' : 'detail-characters-list'})):
+                        actors_list = actors_list.find_all('h3', {'class' : 'h3_characters_voice_actors'})
 
-                    print(anime)
+                        for actor in actors_list:
+                            anime.actors_list.append(actor.text.strip())
+
+                    with open('output/data.csv', 'a', encoding="utf-8") as f:
+                        f.write(str(anime))
+                        f.write('\n')
             
             #print(anime)
+        page += 50
+
+        if page % 250 == 0:
+            time.sleep(20)
 
 if __name__ == "__main__":
     myanilist()
