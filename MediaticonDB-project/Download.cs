57eb2348ca129c026cfc9be4b264bd0bb5804194
Visual Laser 10 New <visualaser10new@gmail.com>
2022-01-28 06:13:18 +0100

    add mediaticon db
    download csv, convert them in db

---
+++ MediaticonDB-project/Download.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaticonDB
+{
+    internal class Download
+    {
+        //check connection
+        //check date online
+        //check date of the last film in db
+        //download all csv from that year to today, put them in ./csv/film/film_2020.csv
+
+        private string fromUrl = @"https://mediaticon.000webhostapp.com//";
+        private string toPath = @".\csv\";
+
+        public static bool downloadAll()
+        {
+            if (!IsOnline())
+                return false;
+
+            string[] tables = { "Film", "Serie", "Anime", "Show" };
+
+            foreach (string table in tables)
+            {
+                //foreach type
+                DateTime today;
+                DateTime lastContent;
+
+                if (!Dater.getDate(out today))
+                        return false;
+                if (!Dater.lastContent(table, out lastContent))
+                    return false;
+
+                if (today.Year >= lastContent.Year)
+                {
+                    //download
+                    for (int i = today.Year; i <= lastContent.Year; i++)
+                    {
+                        //for each year download file
+                        if (!downloadFile(table, i))
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static bool IsOnline()
+        {
+            string host = @"https://www.google.com";
+            bool result = false;
+            Ping p = new Ping();
+            try
+            {
+                PingReply reply = p.Send(host, 3000);
+                if (reply.Status == IPStatus.Success)
+                    return true;
+            }
+            catch { }
+            return result;
+        }
+
+        private class Dater
+        {
+            public static bool getDate(out DateTime date)
+            {
+
+            }
+
+            public static bool lastContent(string table, out DateTime date)
+            {
+
+            }
+        }
+
+        private static bool downloadFile(string type, int year)
+        {
+
+        }
+
+    }
+}
---
+++ MediaticonDB-project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MediaticonDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //link_imgGrande; link_img; titolo; §trama§; durata; anno; ['generi', ]; tipo; ['attori', ]
+            //string c = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+            //string c = @"Server=(localdb)\\MSSQLLocalDb;Integrated Security=true;database=Database1.mdf";
+            //giusta    string c = @"Data Source=(localdb)\MSSQLLocalDb;Integrated Security=true;AttachDbFileName=C:\Users\Visual Laser 10 New\source\repos\MediaticonDB\Database1.mdf;";
+
+
+        }
+
+       
+
+    }
+}
