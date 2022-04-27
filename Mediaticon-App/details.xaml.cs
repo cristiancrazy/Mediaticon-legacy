02cf7cb0a1470eb54393347dad2d6cb12d508667
Visual Laser 10 New <visualaser10new@gmail.com>
2022-04-27 09:58:50 +0200

    fix

--- Mediaticon-App/details.xaml.cs
+++ Mediaticon-App/details.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -21,134 +21,142 @@ using System.Windows.Interop;
 
 namespace Mediaticon
 {
-	/// <summary>
-	/// Interaction logic for details.xaml
-	/// </summary>
-	/// 
+    /// <summary>
+    /// Interaction logic for details.xaml
+    /// </summary>
+    /// 
 
-	public class ProgTV
-	{
-		public ProgTV(string icon, DateTime orainizio, DateTime orafine)
-		{
-			giorno = orainizio.ToString("d");
-			oraInizio = orainizio.ToString("HH:mm");
-			oraFine = orafine.ToString("HH:mm");
-		}
-		public string giorno { set; get; }
-		public string oraInizio { set; get; }
-		public string oraFine { set; get; }
-	}
-
-	public partial class details : Window
-	{
-		static Film? opened = null;
-
-		public details()
-        {
-			InitializeComponent();
-        }
-
-		public details(Film toOpen)
-		{
-			InitializeComponent();
-			setFilm(toOpen);
-			setGUI();
-		}
-
-		private void accountBord_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-		{
-			accountCombo.IsDropDownOpen = !accountCombo.IsDropDownOpen;
-		}
-
-		private void comboClick(object sender, MouseButtonEventArgs e)
-		{
-			seeBtn.IsDropDownOpen = true;
-		}
-
-	}
-
-	public partial class details : Window
+    public class ProgTV
     {
-		void setFilm(Film input)
-		{
-			try
-			{
-				opened = input;
-			}
-			catch
-			{
-				NMSG.Show("Impossibile caricare il contenuto", NMSGtype.Ok);
-				Applicazione.openWindow<MainWindow, details>(Applicazione.CloserType.Close);
-			}
-		}
-
-		void setGUI()
+        public ProgTV(string icon, DateTime orainizio, DateTime orafine)
         {
-			titleLbl.Content = opened.Title;
-			descBox.Text = opened.Description;
-			attoriBox.Text = "Attori: " + opened.Actors.ListToString();
-			durataBox.Text = opened.Duration + " minuti";
-			annoBox.Text = opened.Year.ToString();
-			genereBox.Text = opened.Genres.ListToString();
-			tipoBox.Text = EnviromentVar.Modality.CurrentModality.ToString();
-			makeList();
+            giorno = orainizio.ToString("d");
+            oraInizio = orainizio.ToString("HH:mm");
+            oraFine = orafine.ToString("HH:mm");
+        }
+        public string giorno { set; get; }
+        public string oraInizio { set; get; }
+        public string oraFine { set; get; }
+    }
+
+    public partial class details : Window
+    {
+        static Film? opened = null;
+
+        public details()
+        {
+            InitializeComponent();
         }
 
-		void setCover(Bitmap bitmap)
-		{
-			try
-			{
-				var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
-																			   IntPtr.Zero,
-																			   Int32Rect.Empty,
-																			   BitmapSizeOptions.FromEmptyOptions()
-				);
-				bitmap.Dispose();
-				coverImg.Source = new ImageBrush(bitmapSource).ImageSource;
-			}
-			catch
-			{
-				return;
-			}
-		}
+        public details(Film toOpen)
+        {
+            InitializeComponent();
+            setFilm(toOpen);
+            setGUI();
+        }
 
-		void setBackground(string url)
-		{
-			Bitmap bitmap;
-			try
-			{
-				if (!Connection.DownloadImage(url, out bitmap))
-				{
-					return;
-				}
+        private void accountBord_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            accountCombo.IsDropDownOpen = !accountCombo.IsDropDownOpen;
+        }
 
-				var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
-																				IntPtr.Zero,
-																				Int32Rect.Empty,
-																				BitmapSizeOptions.FromEmptyOptions()
-				);
-				bitmap.Dispose();
-				bkgImg.ImageSource = new ImageBrush(bitmapSource).ImageSource;
-			}
-			catch
-			{
-				return;
-			}
-		}
+        private void comboClick(object sender, MouseButtonEventArgs e)
+        {
+            seeBtn.IsDropDownOpen = true;
+        }
 
-		void makeList()
-		{
-			//provide to get the guidatv
-			List<ProgTV> tVs = new List<ProgTV>();
-			for (int i = 0; i < 10; i++)
-			{
-				tVs.Add(new ProgTV("",
-				  new DateTime(year: 1, month: 1, day: 1, hour: 12, minute: 23 + i, second: 0),
-				  new DateTime(year: 1, month: 1, day: 1, hour: 13, minute: 40 + i, second: 0))
-				  );
-			}
+    }
 
-			listaTV.ItemsSource = tVs;
-		}
-	}
+    public partial class details : Window
+    {
+        void setFilm(Film input)
+        {
+            try
+            {
+                opened = input;
+            }
+            catch
+            {
+                NMSG.Show("Impossibile caricare il contenuto", NMSGtype.Ok);
+                Applicazione.openWindow<MainWindow, details>(Applicazione.CloserType.Close);
+            }
+        }
+
+        void setGUI()
+        {
+            titleLbl.Content = opened.Title;
+            descBox.Text = opened.Description;
+            attoriBox.Text = "Attori: " + opened.Actors.ListToString();
+            durataBox.Text = opened.Duration + " minuti";
+            annoBox.Text = opened.Year.ToString();
+            genereBox.Text = opened.Genres.ListToString();
+            tipoBox.Text = EnviromentVar.Modality.CurrentModality.ToString();
+
+            setCover(opened.Cover);
+            setBackground(opened.BigImage);
+
+            makeList();
+        }
+
+        void setCover(Bitmap bitmap)
+        {
+            try
+            {
+                var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                                                                               IntPtr.Zero,
+                                                                               Int32Rect.Empty,
+                                                                               BitmapSizeOptions.FromEmptyOptions()
+                );
+                bitmap.Dispose();
+                coverImg.Source = new ImageBrush(bitmapSource).ImageSource;
+            }
+            catch
+            {
+
+                Connection.openImage(EnviromentVar.ImagesVar.defaultCoverPath, out bitmap);
+                var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                                                                           IntPtr.Zero,
+                                                                           Int32Rect.Empty,
+                                                                           BitmapSizeOptions.FromEmptyOptions()
+                );
+                bitmap.Dispose();
+                coverImg.Source = new ImageBrush(bitmapSource).ImageSource;
+            }
+        }
+
+        void setBackground(string url)
+        {
+            Bitmap bitmap;
+            try
+            {
+                Connection.DownloadImage(url, out bitmap);
+                var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                                                                                IntPtr.Zero,
+                                                                                Int32Rect.Empty,
+                                                                                BitmapSizeOptions.FromEmptyOptions()
+                );
+                bitmap.Dispose();
+                bkgImg.ImageSource = new ImageBrush(bitmapSource).ImageSource;
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        void makeList()
+        {
+            //provide to get the guidatv
+            List<ProgTV> tVs = new List<ProgTV>();
+            for (int i = 0; i < 10; i++)
+            {
+                tVs.Add(new ProgTV("",
+                  new DateTime(year: 1, month: 1, day: 1, hour: 12, minute: 23 + i, second: 0),
+                  new DateTime(year: 1, month: 1, day: 1, hour: 13, minute: 40 + i, second: 0))
+                  );
+            }
+
+            listaTV.ItemsSource = tVs;
+        }
+    }
 }
